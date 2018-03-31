ca06763f47e67f945700822b4677707d2d13a6b5
haochiencho@gmail.com <haochiencho@gmail.com>
2018-03-31 12:20:50 -0700

    added testing client

--- application.py
+++ application.py
@@ -1,17 +1,19 @@
 from flask import Flask
 from flask import render_template
+from flask import request
 app = Flask(__name__)
 
-@app.route('/add_flight/<username>')
-
-def add_flight(username=None):
+@app.route('/add_flight', methods=['POST'])
+def add_flight():
     # TODO: update database with flight information
     # username in URL and remaining info in POST request
+    username = request.form['username']
 
     return get_dashboard(username)
 
 def check_flights_for_all_users():
     # TODO: pull all users from DB and check flight for each destination
+
     # store flight info in DB
     return None
 
@@ -19,3 +21,7 @@ def get_dashboard(username):
     # TODO: look up destinations in DB and display to user
 
     return render_template('dashboard.html', name=username)
+
+@app.route('/test_client')
+def test_client():
+    return render_template('test_client.html')
