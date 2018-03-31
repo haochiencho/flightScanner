f5b6bfdbfd095dc21e1cb934b9fffc332f1a698b
haochiencho@gmail.com <haochiencho@gmail.com>
2018-03-31 11:15:36 -0700

    basic backend template

--- application.py
+++ application.py
@@ -6,9 +6,13 @@ app = Flask(__name__)
 
 def add_flight(name=None):
     # TODO: update database with flight information
+    # username in URL and remaining info in POST request
 
     return render_template('dashboard.html', name=name)
 
 def check_flights_for_all_users():
     # TODO: pull all users from DB and check flight for each destination
     # store flight info in DB
+
+def get_dashboard(username):
+    # TODO: look up destinations in DB and display to user
