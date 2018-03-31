f0658a201931d0be5fe90bf3af24db718e8ca3f0
haochiencho@gmail.com <haochiencho@gmail.com>
2018-03-31 11:18:57 -0700

    more to template

--- application.py
+++ application.py
@@ -2,17 +2,20 @@ from flask import Flask
 from flask import render_template
 app = Flask(__name__)
 
-@app.route('/add_flight/<name>')
+@app.route('/add_flight/<username>')
 
-def add_flight(name=None):
+def add_flight(username=None):
     # TODO: update database with flight information
     # username in URL and remaining info in POST request
 
-    return render_template('dashboard.html', name=name)
+    return get_dashboard(username)
 
 def check_flights_for_all_users():
     # TODO: pull all users from DB and check flight for each destination
     # store flight info in DB
+    return None
 
 def get_dashboard(username):
     # TODO: look up destinations in DB and display to user
+
+    return render_template('dashboard.html', name=username)
