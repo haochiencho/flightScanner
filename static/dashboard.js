972873662004eb8c2c7d35ddaac8337a9c5d3791
Luca Guarro <luca.guarro@gmail.com>
2018-03-31 21:56:51 -0700

    made some danks tuff

--- static/dashboard.js
+++ static/dashboard.js
@@ -1,206 +1,76 @@
-$(function() {
+console.log("is you running2.0");
+function initAutocomplete() {
+    console.log("init auto completed called");
 
-  $(document).on("click", function(e) {
-    var $item = $(".rad-dropmenu-item");
-    if ($item.hasClass("active")) {
-      $item.removeClass("active");
+    var map = new google.maps.Map(document.getElementById('user-map'), {
+        center: {lat: -33.8688, lng: 151.2195},
+        zoom: 13,
+        mapTypeId: 'roadmap'
+    });
+    console.log("map boys ", map)
+    if (navigator.geolocation) {
+        navigator.geolocation.getCurrentPosition(function (position) {
+            initialLocation = new google.maps.LatLng(position.coords.latitude, position.coords.longitude);
+            map.setCenter(initialLocation);
+        });
     }
-  });
 
-  $(".rad-toggle-btn").on('click', function() {
-    $(".rad-sidebar").toggleClass("rad-nav-min");
-    $(".rad-body-wrapper").toggleClass("rad-nav-min");
-    setTimeout(function() {
-      initializeCharts();
-    }, 200);
-  });
+    // Create the search box and link it to the UI element.
+    var input = document.getElementById('pac-input');
+    var searchBox = new google.maps.places.SearchBox(input);
+    map.controls[google.maps.ControlPosition.TOP_LEFT].push(input);
 
-  $(".rad-dropdown >.rad-menu-item").on('click', function(e) {
-    e.stopPropagation();
-    $(".rad-dropmenu-item").removeClass("active");
-    $(this).next(".rad-dropmenu-item").toggleClass("active");
-  });
-
-  $(window).resize(function() {
-    $.throttle(250, setTimeout(function() {
-      initializeCharts();
-    }, 200));
-  });
-
-  var colors = [
-    '#E94B3B',
-    '#39C7AA',
-    '#1C7EBB',
-    '#F98E33'
-  ];
-
-  var panelList = $('.row');
-
-  panelList.sortable({
-    handle: '.row',
-    update: function() {
-      $('.panel', panelList).each(function(index, elem) {
-        var $listItem = $(elem),
-          newIndex = $listItem.index();
-      });
-    }
-  });
-
-  function initializeCharts() {
-
-    $(".rad-chart").empty();
-
-    Morris.Line({
-      lineColors: colors,
-      element: 'lineChart',
-      data: [{
-        year: '2011',
-        value: 32
-      }, {
-        year: '2012',
-        value: 17
-      }, {
-        year: '2013',
-        value: 41
-      }, {
-        year: '2014',
-        value: 26
-      }, {
-        year: '2015',
-        value: 9
-      }],
-      xkey: 'year',
-      ykeys: ['value'],
-      labels: ['Value']
+    // Bias the SearchBox results towards current map's viewport.
+    map.addListener('bounds_changed', function() {
+        searchBox.setBounds(map.getBounds());
     });
 
-    Morris.Donut({
-      element: 'donutChart',
-      data: [{
-        value: 40,
-        label: 'SS'
-      }, {
-        value: 15,
-        label: 'baz'
-      }, {
-        value: 35,
-        label: 'bar'
-      }, {
-        value: 15,
-        label: 'baz'
-      }, ],
-      labelColor: '#23AE89',
-      colors: colors
+    var markers = [];
+    // Listen for the event fired when the user selects a prediction and retrieve
+    // more details for that place.
+    searchBox.addListener('places_changed', function() {
+        var places = searchBox.getPlaces();
+
+        if (places.length == 0) {
+        return;
+        }
+
+        // Clear out the old markers.
+        markers.forEach(function(marker) {
+        marker.setMap(null);
+        });
+        markers = [];
+
+        // For each place, get the icon, name and location.
+        var bounds = new google.maps.LatLngBounds();
+        places.forEach(function(place) {
+        if (!place.geometry) {
+            console.log("Returned place contains no geometry");
+            return;
+        }
+        var icon = {
+            url: place.icon,
+            size: new google.maps.Size(71, 71),
+            origin: new google.maps.Point(0, 0),
+            anchor: new google.maps.Point(17, 34),
+            scaledSize: new google.maps.Size(25, 25)
+        };
+
+        // Create a marker for each place.
+        markers.push(new google.maps.Marker({
+            map: map,
+            icon: icon,
+            title: place.name,
+            position: place.geometry.location
+        }));
+
+        if (place.geometry.viewport) {
+            // Only geocodes have viewport.
+            bounds.union(place.geometry.viewport);
+        } else {
+            bounds.extend(place.geometry.location);
+        }
+        });
+        map.fitBounds(bounds);
     });
-
-    Morris.Bar({
-      element: 'barChart',
-      data: [{
-        y: 'Jan',
-        a: 55,
-        b: 90,
-        c: 12
-      }, {
-        y: 'Feb',
-        a: 65,
-        b: 15,
-        c: 16
-      }, {
-        y: 'Mar',
-        a: 50,
-        b: 40,
-        c: 05
-      }, {
-        y: 'May',
-        a: 95,
-        b: 65,
-        c: 65
-      }, {
-        y: 'Jun',
-        a: 50,
-        b: 40,
-        c: 20
-      }, {
-        y: 'Jul',
-        a: 75,
-        b: 65,
-        c: 85
-      }, {
-        y: 'Aug',
-        a: 10,
-        b: 90,
-        c: 90
-      }, {
-        y: 'Sep',
-        a: 15,
-        b: 65,
-        c: 07
-      }, {
-        y: 'Oct',
-        a: 75,
-        b: 18,
-        c: 13
-      }, {
-        y: 'Nov',
-        a: 15,
-        b: 65,
-        c: 03
-      }, {
-        y: 'Dec',
-        a: 03,
-        b: 95,
-        c: 02
-      }],
-
-      xkey: 'y',
-      ykeys: ['a', 'b', 'c'],
-      barColors: [
-
-        '#39C7AA',
-        '#1C7EBB',
-        '#E94B3B',
-      ],
-      labels: ['Series ASH', 'Series SS']
-    });
-
-    Morris.Area({
-      element: 'areaChart',
-      lineColors: colors,
-      data: [{
-        y: '2006',
-        a: 100,
-        b: 90
-      }, {
-        y: '2007',
-        a: 75,
-        b: 65
-      }, {
-        y: '2008',
-        a: 50,
-        b: 40
-      }, {
-        y: '2009',
-        a: 75,
-        b: 65
-      }, {
-        y: '2010',
-        a: 50,
-        b: 40
-      }, {
-        y: '2011',
-        a: 75,
-        b: 65
-      }, {
-        y: '2012',
-        a: 100,
-        b: 90
-      }],
-      xkey: 'y',
-      ykeys: ['a', 'b'],
-      labels: ['Series ASH', 'Series SS']
-    });
-
-  }
-
-  initializeCharts();
-});
+}
