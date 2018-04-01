ccd04e4ffc347759c59663c559ac8991e8615336
jjas0nn <jason.jen.2010@gmail.com>
2018-04-01 01:55:26 -0700

    jj

--- deploy.sh
+++ deploy.sh
@@ -2,5 +2,5 @@ git pull
 git add --all
 git commit -m "jj"
 git push origin master
-gcloud compute scp * warthogs:/home/jj/flightscanner --recurse
+gcloud compute scp * warthogs:/var/www/flightscanner --recurse
 
