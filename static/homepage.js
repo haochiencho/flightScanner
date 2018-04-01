337d86e834143fb999ce58782b2838ed2555e988
Luca Guarro <luca.guarro@gmail.com>
2018-03-31 20:18:10 -0700

    added static dashboard layout, and google maps now goes to current location

--- static/homepage.js
+++ static/homepage.js
@@ -1,11 +1,18 @@
 console.log("is you running");
 function initAutocomplete() {
     console.log("init auto completed called");
+
     var map = new google.maps.Map(document.getElementById('map'), {
         center: {lat: -33.8688, lng: 151.2195},
         zoom: 13,
         mapTypeId: 'roadmap'
     });
+    if (navigator.geolocation) {
+        navigator.geolocation.getCurrentPosition(function (position) {
+            initialLocation = new google.maps.LatLng(position.coords.latitude, position.coords.longitude);
+            map.setCenter(initialLocation);
+        });
+    }
 
     // Create the search box and link it to the UI element.
     var input = document.getElementById('pac-input');
